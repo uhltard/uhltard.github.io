08b1bcf50da58f20830c0ade82a1909353e96d24
Spa4r <jeton.alex@pymail.fr>
2022-05-24 21:39:57 +0200

    :lipstick: REMOVE CLIENT INFO

--- pages/projects/_id.vue
+++ pages/projects/_id.vue
@@ -98,43 +98,6 @@ export default {
       <div class="block sm:flex gap-0 sm:gap-10 mt-14">
         <!-- Single project left section details -->
         <div class="w-full sm:w-1/3 text-left">
-          <!-- Single project client details -->
-          <div class="mb-7">
-            <p
-              class="
-                font-general-medium
-                text-2xl text-secondary-dark
-                dark:text-secondary-light
-                mb-2
-              "
-            >
-              {{ project.clientTitle }}
-            </p>
-            <ul class="leading-loose">
-              <li
-                v-for="info in project.companyInfos"
-                :key="info.id"
-                class="
-                  font-general-regular
-                  text-ternary-dark
-                  dark:text-ternary-light
-                "
-              >
-                <span>{{ info.title }}: </span>
-                <a
-                  href="#"
-                  :class="
-                    info.title == 'Website' || info.title == 'Phone'
-                      ? 'hover:underline cursor-pointer'
-                      : ''
-                  "
-                  aria-label="Project website and phone"
-                  >{{ info.details }}</a
-                >
-              </li>
-            </ul>
-          </div>
-
           <!-- Single project objectives -->
           <div class="mb-7">
             <p
