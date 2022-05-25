f3a58a2d3345f1dafb878936a99b15d008aa19bb
Spa4r <jeton.alex@pymail.fr>
2022-05-25 15:35:55 +0200

    :technologist: ADD github deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: "static",
   router: {
-    base: '/uhltard.github.io/'
+    base: '/'
   },
   colorMode: {
     classSuffix: "",
