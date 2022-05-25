91874da3d936269eb6d54c49554d6e51e6645122
Alex J <94291987+uhltard@users.noreply.github.com>
2022-05-25 22:38:29 +0200

    :recycle:  REMOVE  : projects pages (#12)

--- components/shared/AppNavigation.vue
+++ components/shared/AppNavigation.vue
@@ -42,58 +42,6 @@ export default {
       aria-label="About Me"
       >À propos de moi</NuxtLink
     >
-    <!--
-    <NuxtLink
-      to="/contact"
-      class="
-        block
-        text-left text-lg text-primary-dark
-        dark:text-ternary-light
-        hover:text-secondary-dark
-        dark:hover:text-secondary-light
-        sm:mx-4
-        mb-2
-        sm:py-2
-        border-t-2
-        pt-3
-        sm:pt-2 sm:border-t-0
-        border-primary-light
-        dark:border-secondary-dark
-      "
-      aria-label="Contact"
-      >Contact</NuxtLink
-    >
-    -->
-    <div
-      class="
-        font-general-regular
-        border-t-2
-        pt-3
-        sm:pt-0 sm:border-t-0
-        border-primary-light
-        dark:border-secondary-dark
-      "
-    >
-      <button
-        class="
-          sm:hidden
-          block
-          text-left text-md
-          bg-indigo-500
-          hover:bg-indigo-600
-          text-white
-          shadow-sm
-          rounded-md
-          px-4
-          py-2
-          mt-2
-        "
-        @click="showModal()"
-        aria-label="Hire Me Button"
-      >
-        Hire Me
-      </button>
-    </div>
   </div>
 </template>
 
