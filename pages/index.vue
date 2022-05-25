8fd56826a743bd78b25c6e292a52e77f42e3509d
Alex J <94291987+uhltard@users.noreply.github.com>
2022-05-25 22:15:57 +0200

    :pencil2:  REMOVE  : projects pages (#10)

--- pages/index.vue
+++ pages/index.vue
@@ -13,31 +13,6 @@ export default {
 
     <ProjectsGrid />
 
-    <!-- View more projects button -->
-    <div class="mt-5 sm:mt-14 flex justify-center">
-      <NuxtLink
-        to="/projects"
-        class="
-          font-general-medium
-          flex
-          items-center
-          px-6
-          py-3
-          rounded-lg
-          shadow-lg
-          hover:shadow-xl
-          bg-indigo-500
-          hover:bg-indigo-600
-          focus:ring-1 focus:ring-indigo-900
-          text-white text-lg
-          sm:text-xl
-          duration-300
-        "
-        aria-label="D'autres projets"
-      >
-        <Button title="D'autres projets" />
-      </NuxtLink>
-    </div>
   </div>
 </template>
 
