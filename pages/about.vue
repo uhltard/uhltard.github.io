f819971ae6236a7f76f665a6b4bcbd846314aec8
Spa4r <jeton.alex@pymail.fr>
2022-05-25 21:24:51 +0200

    :pencil2:  DELETE : AboutCounter.vue

--- pages/about.vue
+++ pages/about.vue
@@ -1,23 +1,13 @@
 <script>
-import feather from "feather-icons";
-
 export default {
   name: "About",
   scrollToTop: true,
-  mounted() {
-    feather.replace();
-  },
-  updated() {
-    feather.replace();
-  },
 };
 </script>
 
 <template>
   <div>
     <AboutMe class="container mx-auto" />
-
-    <AboutCounter />
 
     <AboutClients class="container mx-auto" />
   </div>
