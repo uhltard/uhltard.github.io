f39668dbd185ee3558540589593ea5bc8b03f865
Alex J <94291987+uhltard@users.noreply.github.com>
2022-06-06 14:23:04 +0200

    Refactor about v4 (#17)
    * :fire: remove AppNavigatiion
    
    * :fire: remove AboutCounter.vue && about.vue
    
    * :fire: UPDATE About.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,28 @@
 <script>
 import AppBanner from "../components/shared/AppBanner.vue";
 import Button from "../components/reusable/Button.vue";
+import feather from "feather-icons";
+
 export default {
   scrollToTop: true,
   components: { AppBanner, Button },
+  mounted() {
+    feather.replace();
+  },
+  updated() {
+    feather.replace();
+  },
 };
 </script>
 
 <template>
   <div class="container mx-auto">
+
     <AppBanner />
+
+    <AboutMe />
+
+    <AboutClients />
 
     <ProjectsGrid />
 
